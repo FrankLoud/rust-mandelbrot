cbd8d90bf060aab6b68676246a5539c8c79ad9b4
Julio <julio.olvr@gmail.com>
2019-03-16 09:49:12 -0300

    escape_time fn

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+extern crate num;
+use num::Complex;
 use std::str::FromStr;
 
 fn main() {
@@ -21,4 +23,24 @@ fn test_parse_pair() {
     assert_eq!(parse_pair::<i32>("", ','), None);
     assert_eq!(parse_pair("12x14", 'x'), Some((12, 14)));
     assert_eq!(parse_pair("1.5,2", ','), Some((1.5, 2.0)));
+}
+
+fn escape_time(c: Complex<f64>, limit: u32) -> Option<u32> {
+    let mut z = Complex { re: 0.0, im: 0.0 };
+
+    for i in 0..limit {
+        z = z * z + c;
+        if z.norm_sqr() >= 4.0 {
+            return Some(i);
+        }
+    }
+
+    return None;
+}
+
+#[test]
+fn test_escape_time() {
+    assert_eq!(escape_time(Complex { re: 3.0, im: 4.0 }, 255), Some(0));
+    assert_eq!(escape_time(Complex { re: 0.1, im: 0.8 }, 255), Some(5));
+    assert_eq!(escape_time(Complex { re: 0.1, im: 0.8 }, 3),   None);
 }
