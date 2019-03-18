d47d7a4d57e174e72e05adb695753338889c4ffc
Julio <julio.olvr@gmail.com>
2019-03-18 09:12:38 -0300

    Multithreaded version

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+extern crate crossbeam;
 extern crate image;
 extern crate num;
 
@@ -30,7 +31,28 @@ fn main() -> Result<(), Box<std::error::Error>> {
 
     let mut pixels = vec![0; bounds.0 * bounds.1];
 
+    let threads = 8;
+    let rows_per_band = bounds.1 / threads + 1;
+    let bands: Vec<&mut [u8]> = pixels.chunks_mut(rows_per_band * bounds.0).collect();
+
+    crossbeam::scope(|spawner| {
+        for (i, band) in bands.into_iter().enumerate() {
+            let top = rows_per_band * i;
+            let height = band.len() / bounds.0;
+            let band_bounds = (bounds.0, height);
+            let band_upper_left = pixel_to_point(bounds, (0, top), upper_left, lower_right);
+            let band_lower_right =
+                pixel_to_point(bounds, (bounds.0, top + height), upper_left, lower_right);
+
+            spawner.spawn(move |_| {
+                render(band, band_bounds, band_upper_left, band_lower_right);
+            });
+        }
+    })
+    .expect("An error happened in one of the threads");
+
     render(&mut pixels, bounds, upper_left, lower_right);
+
     write_image(&args[1], &pixels, bounds).expect("Error writing image file");
 
     Ok(())
